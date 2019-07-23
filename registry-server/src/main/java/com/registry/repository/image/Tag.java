fd006b9b6e402dbfa3ef42aeb3ca5dc7d705dba0
boozer <boozer@exntu.com>
2019-07-23 18:49:16 +0900

    postgresql 로 변경

--- registry-server/src/main/java/com/registry/repository/image/Tag.java
+++ registry-server/src/main/java/com/registry/repository/image/Tag.java
@@ -30,45 +30,45 @@ public class Tag implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	/** image */
-  	@Column(name = "image", columnDefinition="varchar(40)", nullable=false)
-  	private String image;
+    /** image */
+    @Column(name = "image_key", columnDefinition="varchar(40)", nullable=false)
+    private String image_key;
 
-	/** manifest digest */
-	@Column(name = "manifest_digest", columnDefinition="varchar(255)")
-	private String manifestDigest;
+    /** manifest digest */
+    @Column(name = "manifest_digest", columnDefinition="varchar(255)")
+    private String manifestDigest;
 
-	/** 이름 */
-	@Column(name = "name", columnDefinition="varchar(40)")
-	private String name;
+    /** 이름 */
+    @Column(name = "name", columnDefinition="varchar(40)")
+    private String name;
 
-	/** size */
-	@Column(name = "size")
-	private Long size;
+    /** size */
+    @Column(name = "size")
+    private Long size;
 
-	/** expiration */
-	@Column(name = "expiration")
-	@Type(type = "org.hibernate.type.LocalDateTimeType")
-	private LocalDateTime expiration;
+    /** expiration */
+    @Column(name = "expiration")
+    @Type(type = "org.hibernate.type.LocalDateTimeType")
+    private LocalDateTime expiration;
 
-	/** start ts */
-	@Column(name = "start_ts")
-	@Type(type = "org.hibernate.type.LocalDateTimeType")
-	private LocalDateTime startTs;
+    /** start ts */
+    @Column(name = "start_ts")
+    @Type(type = "org.hibernate.type.LocalDateTimeType")
+    private LocalDateTime startTs;
 
-	/** end ts */
-	@Column(name = "end_ts")
-	@Type(type = "org.hibernate.type.LocalDateTimeType")
-	private LocalDateTime endTs;
+    /** end ts */
+    @Column(name = "end_ts")
+    @Type(type = "org.hibernate.type.LocalDateTimeType")
+    private LocalDateTime endTs;
 
-	/** build id */
-	@Column(name = "build_id")
-	private Long buildId;
+    /** build id */
+    @Column(name = "build_id")
+    private Long buildId;
 
 	@JsonIgnore
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="image_id", nullable = false)
-	private Image imageEntity;
+	private Image image;
 
 	/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 	| Public Variables
@@ -84,85 +84,85 @@ public class Tag implements Serializable {
 	| Getter & Setter Method ( DI Method )
 	|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getImage() {
-		return image;
-	}
+    public String getImage_key() {
+        return image_key;
+    }
 
-	public void setImage(String image) {
-		this.image = image;
-	}
+    public void setImage_key(String image_key) {
+        this.image_key = image_key;
+    }
 
-	public String getManifestDigest() {
-		return manifestDigest;
-	}
+    public String getManifestDigest() {
+        return manifestDigest;
+    }
 
-	public void setManifestDigest(String manifestDigest) {
-		this.manifestDigest = manifestDigest;
-	}
+    public void setManifestDigest(String manifestDigest) {
+        this.manifestDigest = manifestDigest;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Long getSize() {
-		return size;
-	}
+    public Long getSize() {
+        return size;
+    }
 
-	public void setSize(Long size) {
-		this.size = size;
-	}
+    public void setSize(Long size) {
+        this.size = size;
+    }
 
-	public LocalDateTime getExpiration() {
-		return expiration;
-	}
+    public LocalDateTime getExpiration() {
+        return expiration;
+    }
 
-	public void setExpiration(LocalDateTime expiration) {
-		this.expiration = expiration;
-	}
+    public void setExpiration(LocalDateTime expiration) {
+        this.expiration = expiration;
+    }
 
-	public LocalDateTime getStartTs() {
-		return startTs;
-	}
+    public LocalDateTime getStartTs() {
+        return startTs;
+    }
 
-	public void setStartTs(LocalDateTime startTs) {
-		this.startTs = startTs;
-	}
+    public void setStartTs(LocalDateTime startTs) {
+        this.startTs = startTs;
+    }
 
-	public LocalDateTime getEndTs() {
-		return endTs;
-	}
+    public LocalDateTime getEndTs() {
+        return endTs;
+    }
 
-	public void setEndTs(LocalDateTime endTs) {
-		this.endTs = endTs;
-	}
+    public void setEndTs(LocalDateTime endTs) {
+        this.endTs = endTs;
+    }
 
-	public Long getBuildId() {
-		return buildId;
-	}
+    public Long getBuildId() {
+        return buildId;
+    }
 
-	public void setBuildId(Long buildId) {
-		this.buildId = buildId;
-	}
+    public void setBuildId(Long buildId) {
+        this.buildId = buildId;
+    }
 
-	public Image getImageEntity() {
-		return imageEntity;
-	}
+    public Image getImage() {
+        return image;
+    }
 
-	public void setImageEntity(Image imageEntity) {
-		this.imageEntity = imageEntity;
-	}
+    public void setImage(Image image) {
+        this.image = image;
+    }
 
 	/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 	| Public Method
