5908e041e17fa0c2217d56be0b21e3c6b56a8db1
boozer <boozer@exntu.com>
2019-08-01 11:22:10 +0900

    source 정리

--- registry-server/src/main/java/com/registry/scheduler/ExecutionSyncScheduler.java
+++ registry-server/src/main/java/com/registry/scheduler/ExecutionSyncScheduler.java
@@ -15,7 +15,7 @@ public class ExecutionSyncScheduler {
 
     // 실행에 대한 결과 등과 같은 정보 동기화 모듈 (10분 마다)
     @Scheduled(fixedDelay = 1000 * 60 * 10)
-    public void syndWithBuilder() {
+    public void syncWithBuilder() {
         externalService.syncWithBuilder();
     }
 }
