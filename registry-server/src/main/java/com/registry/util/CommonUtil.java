de5fe1b9e2d91a37da8997e9eea916acdda44d4f
EXNTU <startlkj@exntu.com>
2019-08-23 16:40:14 +0900

    Dockerfile 작업

--- registry-server/src/main/java/com/registry/util/CommonUtil.java
+++ registry-server/src/main/java/com/registry/util/CommonUtil.java
@@ -23,8 +23,26 @@ public class CommonUtil {
         try {
             HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
             ip = req.getHeader("X-FORWARDED-FOR");
-            if (ip == null)
+
+            if (ip == null) {
+                ip = req.getHeader("Proxy-Client-IP");
+            }
+
+            if (ip == null) {
+                ip = req.getHeader("WL-Proxy-Client-IP");
+            }
+
+            if (ip == null) {
+                ip = req.getHeader("HTTP_CLIENT_IP");
+            }
+
+            if (ip == null) {
+                ip = req.getHeader("HTTP_X_FORWARDED_FOR");
+            }
+
+            if (ip == null) {
                 ip = req.getRemoteAddr();
+            }
 
         } catch (Exception e) {
             throw new RuntimeException(e);
