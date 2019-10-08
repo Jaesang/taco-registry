22d2599e3f85c9fbbebacbfdd46f02411944ea44
EXNTU 이경진 <exntu-metatron@googlegroups.com>
2019-10-08 10:45:49 +0900

    time zone 관련 수정

--- registry-server/src/main/java/com/registry/RegistryApplication.java
+++ registry-server/src/main/java/com/registry/RegistryApplication.java
@@ -2,6 +2,7 @@ package com.registry;
 
 import com.registry.config.ApplicationConfig;
 import com.registry.config.RibbonConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,10 +10,20 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication(scanBasePackageClasses = {ApplicationConfig.class})
 @RibbonClient(name = "builder", configuration = RibbonConfig.class)
 @EnableScheduling
 public class RegistryApplication extends SpringBootServletInitializer {
+
+	private @Value("${config.time-zone}") String builderKey;
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistryApplication.class, args);
